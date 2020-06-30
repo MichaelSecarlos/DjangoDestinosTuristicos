3daede2f32d2ceba0317284f05019e8a07ca4b30
Michael Secarlos AC <mapazacals@unsa.edu.pe>
2020-06-30 17:49:42 -0500

    Se crearon las vistas, para el inicio y cerrado de sesion, ademas de añadir la direccion a las urls de la pagina.

--- cuenta/urls.py
+++ cuenta/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('registro/' , views.registro , name='Registrarse')
+    path('registro' , views.registro , name='Registrarse'),
+    path('login' , views.login , name='Login'),
+    path('logout' , views.logout , name='Logout')
 ]
--- cuenta/views.py
+++ cuenta/views.py
@@ -14,17 +14,38 @@ def registro(request):
         if password1 == password2:
             if User.objects.filter(username=username):
                 messages.info(request , "El nombre de usuario ya esta ocupado.")
-                return redirect('.')
+                return redirect('./registro')
     
             elif User.objects.filter(email=email):
                 messages.info(request , "La direccion de correo ya esta ocupada.")
-                return redirect('.')
+                return redirect('./registro')
             else:
                 usuario = User.objects.create_user(username=username , password=password1 , email=email, first_name=first_name, last_name=last_name)
                 usuario.save()
         else:
             messages.info(request , "Las contraseñas no coinciden.")
-            return redirect('.')
+            return redirect('./registro')
         return redirect('/')
     else:
         return render(request , 'registro.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        #Verificacion del usuario, su nombre y contraseña
+        user = auth.authenticate(username=username , password=password)
+
+        #Logueo del usuario, si es que existe
+        if user is not None:
+            auth.login(request , user)
+            return redirect('/')
+        else:
+            messages.info(request , "Usuario o contraseña incorrectos.")
+            return redirect('./login')
+    else:
+        return render(request , 'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect("/")
