ff598a142efba955c133a86f30ce74b976d00328
Michael Secarlos AC <mapazacals@unsa.edu.pe>
2020-06-30 16:25:18 -0500

    Se agregaron las restricciones de contraseña, repeticion de correo o nombre de usuario a la vista del formulario.

--- cuenta/views.py
+++ cuenta/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.models import User , auth
 # Create your views here.
 def registro(request):
@@ -10,9 +11,20 @@ def registro(request):
         password1 = request.POST['password1']
         password2 = request.POST['password2']
         
-        usuario = User.objects.create_user(username=username , password=password1 , email=email, first_name=first_name, last_name=last_name)
-        usuario.save()
+        if password1 == password2:
+            if User.objects.filter(username=username):
+                messages.info(request , "El nombre de usuario ya esta ocupado.")
+                return redirect('.')
+    
+            elif User.objects.filter(email=email):
+                messages.info(request , "La direccion de correo ya esta ocupada.")
+                return redirect('.')
+            else:
+                usuario = User.objects.create_user(username=username , password=password1 , email=email, first_name=first_name, last_name=last_name)
+                usuario.save()
+        else:
+            messages.info(request , "Las contraseñas no coinciden.")
+            return redirect('.')
         return redirect('/')
     else:
-        print("aqui we -_-")
         return render(request , 'registro.html')
