4c2f584b6d4ca4dee418f769a56d7a687bf5572f
Michael Secarlos AC <mapazacals@unsa.edu.pe>
2020-07-06 20:24:14 -0500

    Se añadio la vista destinosRegister, para mostrar el formulario para el registro de nuevos destinos.

--- travello/views.py
+++ travello/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from .models import DestinosTuristicos
+from .forms import DestinosForm
 
 # Create your views here.
 def index(request):
     destinos = DestinosTuristicos.objects.all()
     return render(request , 'index.html' , {'destinos':destinos})
+
+#Registro de los nuevos destinos turisticos
+def destinosRegister(request):
+    if request.method == 'POST':
+        form = DestinosForm(request.POST or None, request.FILES)
+        if form.is_valid():
+            form.save()
+            form = DestinosForm()
+    else:
+        form = DestinosForm(request.POST or None)
+    context = {
+        'form':form
+    }
+    return render(request , 'registroDestinos.html' , context)
