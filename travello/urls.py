933ea93de6d8c4b23e0416ab8af015793760c466
Michael Secarlos AC <mapazacals@unsa.edu.pe>
2020-07-07 18:29:57 -0500

    Se añadio la url para la eliminacion de un destino

--- travello/urls.py
+++ travello/urls.py
@@ -4,5 +4,6 @@ urlpatterns = [
     path('' , views.index , name='index'),
     path('registro' , views.destinosRegister , name='registrarDestino'), #La vista para el registro del destino
     path('listar' , views.destinosLista , name='listarDestinos'), #La vista para listar los destinos
-    path('editar/<int:id_destino>' , views.destinosEdit , name='editarRegistro'), #La vista para editar los destinos
+    path('editar/<int:id_destino>' , views.destinosEdit , name='editarRegistro'), #La vista para editar el destino
+    path('eliminar/<int:id_destino>' , views.destinosEliminar , name='eliminarDestino') #Vista para eliminar el destino
 ]
