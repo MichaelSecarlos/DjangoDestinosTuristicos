97ffbecdf6cd8f7a13909668bfe5c9ad93d0d245
Michael Secarlos AC <mapazacals@unsa.edu.pe>
2020-07-06 20:27:30 -0500

    Se agrego la direccion del formulario para el registro de nuevos destinos

--- travello/urls.py
+++ travello/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views 
 urlpatterns = [
-    path('' , views.index , name='index')
+    path('' , views.index , name='index'),
+    path('registro' , views.destinosRegister , name='registrarDestino') #La vista para el registro del destino
 ]
