df389fddf6492794b5934b4e3ea0c1197c773523
Michael Secarlos AC <mapazacals@unsa.edu.pe>
2020-07-07 09:06:07 -0500

    Se agrego la direccion a la pagina para listar las ciudades y editarlas

--- travello/urls.py
+++ travello/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views 
 urlpatterns = [
     path('' , views.index , name='index'),
-    path('registro' , views.destinosRegister , name='registrarDestino') #La vista para el registro del destino
+    path('registro' , views.destinosRegister , name='registrarDestino'), #La vista para el registro del destino
+    path('listar' , views.destinosLista , name='listarDestinos'), #La vista para listar los destinos
+    path('editar/<int:id_destino>' , views.destinosEdit , name='editarRegistro'), #La vista para editar los destinos
 ]
